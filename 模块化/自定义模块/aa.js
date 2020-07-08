0461084b8e424a1c115573f69d44ea8a251e61bc
rsrl02021002 <1052657260@qq.com>
2020-07-08 09:01:31 +0800

    自定义模块

---
+++ 模块化/自定义模块/aa.js
@@ -0,0 +1,24 @@
+// var name = 'Danny'
+// var age = 20
+// function say() {
+//     console.log(name)
+//     console.log("早上好")
+// }
+// say()
+//
+// module.exports = {
+//     name,
+//     sge,
+//     say
+// }
+let obj = {
+    name:'Danny',
+    age:20,
+    say(){
+        // 暂时不能使用参数
+        // console.log(name)
+        console.log("早上好")
+    }
+}
+
+module.exports = obj
---
+++ 模块化/自定义模块/bb.js
@@ -0,0 +1,5 @@
+// var t = require('./aa')
+// console.log(t.name)
+var ModuleA = require('./aa')
+// console.log(ModuleA)
+ModuleA.say()
